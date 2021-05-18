fdada4ec4885c60d0f37aad054413b002acc8d0a
Donny Roufs <iamchets@yahoo.com>
2021-05-18 23:39:51 +0200

    remove obsolete code

--- server/src/application.ts
+++ server/src/application.ts
@@ -113,15 +113,6 @@ export class Application extends Kondah {
 
     await prisma.connect()
 
-    energizor.get(StrategyService).create({
-      alliesFactionId: 3,
-      axisFactionId: 2,
-      factionId: 2,
-      mapId: 1,
-      title: 'testing',
-      userId: 1,
-    })
-
     server.run(5000)
   }
 }
