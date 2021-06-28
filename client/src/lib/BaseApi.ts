44091df09c51afa2dabad492d648ec3ff45478f3
Donny Roufs <iamchets@yahoo.com>
2021-06-28 21:13:22 +0200

    add prefix back

--- client/src/lib/BaseApi.ts
+++ client/src/lib/BaseApi.ts
@@ -13,6 +13,6 @@ export abstract class BaseApi {
   abstract prefix: string
 
   protected endpoint(endpoint: string = '/') {
-    return '/api/v1' + this.prefix + endpoint
+    return process.env.REACT_APP_BASE_URL + '/api/v1' + this.prefix + endpoint
   }
 }
